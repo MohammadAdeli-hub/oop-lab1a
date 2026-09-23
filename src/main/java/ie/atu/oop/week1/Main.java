1e87ef219e317d668f04de7357be9d4605fc82b8
MohammadAdeli-STUDEN <g00483741@atu.ie>
2026-09-23 16:39:41 +0100

    created second instance of book class and the object is calledd mysecod book

--- src/main/java/ie/atu/oop/week1/Main.java
+++ src/main/java/ie/atu/oop/week1/Main.java
@@ -10,10 +10,23 @@ public class Main {
         myBook.title = "OOP";
         myBook.author = "Fraklin O Connor";
         myBook.pageCount= 412;
+        myBook.available = true;
         System.out.printf("the book is \n"+myBook.title);
         System.out.println("the author is\n "+myBook.author   );
         System.out.println("the page count is \n"+myBook.pageCount);
         System.out.println("the available is \n"+myBook.available);
+
+
+        Book myBook2 = new Book();
+        myBook2.title = "the little programmer";
+        myBook2.author = "Jake Hooper";
+        myBook2.pageCount = 221;
+        myBook2.available = true;
+        System.out.println("the book is \n"+myBook2.title);
+        System.out.println("the author is\n "+myBook2.author   );
+        System.out.println("the page count is \n"+myBook2.pageCount);
+        System.out.println("the available is \n"+myBook2.available);
+
 
     }
 }
