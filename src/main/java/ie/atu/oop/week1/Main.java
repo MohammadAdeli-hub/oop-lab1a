0b106541115b480c17c523eeb02c3727526c0151
MohammadAdeli-STUDEN <g00483741@atu.ie>
2026-09-23 16:29:39 +0100

    created the class and created an instance in main

--- src/main/java/ie/atu/oop/week1/Main.java
+++ src/main/java/ie/atu/oop/week1/Main.java
@@ -6,6 +6,14 @@ public class Main {
     public static void main(String[] args)
     {
         System.out.println("hellow oop");
+        Book myBook = new Book();
+        myBook.title = "OOP";
+        myBook.author = "Fraklin O Connor";
+        myBook.pageCount= 412;
+        System.out.printf("the book is \n"+myBook.title);
+        System.out.println("the author is\n "+myBook.author   );
+        System.out.println("the page count is \n"+myBook.pageCount);
+        System.out.println("the available is \n"+myBook.available);
 
     }
 }
